0f443bf71d596788bc9f1e9d62f9573cf09714c4
k2kdisk <k2kdisk@users.noreply.github.com>
2020-04-10 08:47:00 +0900

    Add main plane estimation class

---
+++ PlaneEstimation/k2kMath3D.cs
@@ -0,0 +1,316 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace k2kLib
+{
+	public class k2kMath3D
+	{
+		public static double[] GetIntersection (double x1, double y1, double z1, double x2, double y2, double z2, double a, double b, double c)
+		{
+			var dx = x2 - x1;
+			var dy = y2 - y1;
+			var dz = z2 - z1;
+			if (a * dx + b * dy - dz == 0)
+				return null;
+
+			var t = (a * x1 + b * y1 + c - z1) / (dz - a * dx - b * dy);
+			return new double[] { x1 + t * dx, y1 + t * dy, z1 + t * dz };
+		}
+
+		public static double[] GetIntersection (double x1, double y1, double z1, double x2, double y2, double z2, double[] plane)
+		{
+			return GetIntersection (x1, y1, z1, x2, y2, z2, plane [0], plane [1], plane [2]);
+		}
+
+		public static double[] GetIntersection (double[] p, double[] q, double[] plane)
+		{
+			return GetIntersection (p [0], p [1], p [2], q [0], q [1], q [2], plane [0], plane [1], plane [2]);
+		}
+
+		public static k2kPoint3D GetIntersection (k2kPoint3D p, k2kPoint3D q, double[] plane)
+		{
+			return GetIntersection (p, q, plane [0], plane [1], plane [2]);
+		}
+		public static k2kPoint3D GetIntersection (k2kPoint3D p, k2kPoint3D q, double a, double b, double c)
+		{
+			return new k2kPoint3D (GetIntersection (p.X, p.Y, p.Z, q.X, q.Y, q.Z, a, b, c));
+		}
+	}
+
+
+	public class k2kPoint3D
+	{
+		public static k2kPoint3D ZeroPoint;
+		static k2kPoint3D ()
+		{
+			ZeroPoint = new k2kPoint3D (0, 0, 0);
+		}
+
+		public double X;
+		public double Y;
+		public double Z;
+
+		public float Xf { get { return (float)X; } }
+		public float Yf { get { return (float)Y; } }
+		public float Zf { get { return (float)Z; } }
+		public int Xi { get { return (int)X; } }
+		public int Yi { get { return (int)Y; } }
+		public int Zi { get { return (int)Z; } }
+
+		public k2kPoint3D (double x, double y, double z)
+		{
+			X = x;
+			Y = y;
+			Z = z;
+		}
+		public k2kPoint3D (double[] coord)
+		{
+			X = coord [0];
+			Y = coord [1];
+			Z = coord [2];
+		}
+		public k2kPoint3D (float x, float y, float z)
+		{
+			X = x;
+			Y = y;
+			Z = z;
+		}
+		public k2kPoint3D (float[] coord)
+		{
+			X = coord [0];
+			Y = coord [1];
+			Z = coord [2];
+		}
+		public k2kPoint3D (int x, int y, int z)
+		{
+			X = x;
+			Y = y;
+			Z = z;
+		}
+		public k2kPoint3D (int[] coord)
+		{
+			X = coord [0];
+			Y = coord [1];
+			Z = coord [2];
+		}
+
+		public static k2kPoint3D operator * (k2kPoint3D p, double k)
+		{
+			return new k2kPoint3D (p.X * k, p.Y * k, p.Z * k);
+		}
+		public static k2kPoint3D operator * (double k, k2kPoint3D p)
+		{
+			return p * k;
+		}
+		public static k2kPoint3D operator / (k2kPoint3D p, double k)
+		{
+			return p * (1 / k);
+		}
+		public static k2kPoint3D operator - (k2kPoint3D p)
+		{
+			return p * (-1);
+		}
+		public static k2kPoint3D operator + (k2kPoint3D p1, k2kPoint3D p2)
+		{
+			return new k2kPoint3D (p1.X + p2.X, p1.Y + p2.Y, p1.Z + p2.Z);
+		}
+		public static k2kPoint3D operator - (k2kPoint3D p1, k2kPoint3D p2)
+		{
+			return p1 + (p2 * (-1));
+		}
+
+		public double Dot (k2kPoint3D vec)
+		{
+			return X * vec.X + Y * vec.Y + Z * vec.Z;
+		}
+
+		public double Norm { get { return k2kMath.GetDistance (X, Y, Z); } }
+		public k2kPoint3D GetNormalized ()
+		{
+			return this / this.Norm;
+		}
+
+		public k2kPoint3D GetComponent (k2kPoint3D DirectionVector)
+		{
+			return DirectionVector * Dot (DirectionVector) /
+				(
+                        DirectionVector.X * DirectionVector.X +
+				DirectionVector.Y * DirectionVector.Y +
+				DirectionVector.Z * DirectionVector.Z
+                    );
+		}
+		public k2kPoint3D GetComponentByUnit (k2kPoint3D DirectionUnitVector)
+		{
+			return DirectionUnitVector * Dot (DirectionUnitVector);
+		}
+
+		public override string ToString ()
+		{
+			return string.Format ("(\t{0}\t,\t{1}\t,\t{2}\t)", X, Y, Z);
+		}
+
+		public double[] ToArray ()
+		{
+			return new double[] { X, Y, Z };
+		}
+		public PointF ToPointF ()
+		{
+			return new PointF (Xf, Yf);
+		}
+		public Point ToPoint ()
+		{
+			return new Point (Xi, Yi);
+		}
+
+
+
+
+		public static double[][] GetRotationMatrixZYX (double Omega, double Phi, double Kappa)
+		{
+			double so = Math.Sin (-Omega);
+			double co = Math.Cos (-Omega);
+			double sp = Math.Sin (-Phi);
+			double cp = Math.Cos (-Phi);
+			double sk = Math.Sin (-Kappa);
+			double ck = Math.Cos (-Kappa);
+			return new double[][]{
+                                    new double[]{ck * cp,-sk * cp,sp},
+                                    new double[]{ck * so * sp + sk * co,ck * co - sk * so * sp,-so * cp},
+                                    new double[]{sk * so - ck * co * sp,sk * co * sp + ck * so,co * cp}
+                                };
+		}
+		public static double[][] GetRotationMatrixXYZ (double Omega, double Phi, double Kappa)
+		{
+			throw new NotImplementedException ();
+			double so = Math.Sin (-Omega);
+			double co = Math.Cos (-Omega);
+			double sp = Math.Sin (-Phi);
+			double cp = Math.Cos (-Phi);
+			double sk = Math.Sin (-Kappa);
+			double ck = Math.Cos (-Kappa);
+			return new double[][]{
+                                    new double[]{1,1,1},
+                                    new double[]{1,1,1},
+                                    new double[]{1,1,1}
+                                };
+		}
+
+		public k2kPoint3D GetRotate (double[][] RotationMatrix)
+		{
+			var ret = new double[3];
+			var vec = new double[] { this.X, this.Y, this.Z };
+			int i, j;
+
+			for (i = 0; i < 3; ++i) {
+				ret [i] = 0;
+				for (j = 0; j < 3; ++j)
+					ret [i] += RotationMatrix [i] [j] * vec [j];
+			}
+			return new k2kPoint3D (ret);
+		}
+
+		public k2kPoint3D GetRotateZYX (double Omega, double Phi, double Kappa)
+		{
+			return GetRotate (GetRotationMatrixZYX (Omega, Phi, Kappa));
+		}
+		public k2kPoint3D GetRotateXYZ (double Omega, double Phi, double Kappa)
+		{
+			return GetRotate (GetRotationMatrixXYZ (Omega, Phi, Kappa));
+		}
+	}
+
+
+
+	public class k2kRotation3D
+	{
+		public k2kRotation3D () : this(0, 0, 0)
+		{
+		}
+		public k2kRotation3D (double Omega, double Phi, double Kappa)
+		{
+			this.Omega = Omega;
+			this.Phi = Phi;
+			this.Kappa = Kappa;
+
+			so = Math.Sin (-Omega);
+			co = Math.Cos (-Omega);
+			sp = Math.Sin (-Phi);
+			cp = Math.Cos (-Phi);
+			sk = Math.Sin (-Kappa);
+			ck = Math.Cos (-Kappa);
+
+			MatrixZYX = new double[][]{  new double[]{ck * cp,-sk * cp,sp},
+                                        new double[]{ck * so * sp + sk * co,ck * co - sk * so * sp,-so * cp},
+                                        new double[]{sk * so - ck * co * sp,sk * co * sp + ck * so,co * cp}};
+		}
+
+		protected double Omega, Phi, Kappa;
+		protected double so, co, sp, cp, sk, ck;
+		protected double[][] MatrixZYX, MatrixXYZ;
+
+		public double[][] RotationMatrixZYX { get { return MatrixZYX; } }
+		public double[][] RotationMatrixXYZ { get { return MatrixXYZ; } }
+
+		protected k2kPoint3D Rotate (k2kPoint3D vec, double[][] RotationMatrix)
+		{
+			var ret = new double[3];
+			var tmp = new double[] { vec.X, vec.Y, vec.Z };
+			int i, j;
+
+			for (i = 0; i < 3; ++i) {
+				ret [i] = 0;
+				for (j = 0; j < 3; ++j)
+					ret [i] += RotationMatrix [i] [j] * tmp [j];
+			}
+			return new k2kPoint3D (ret);
+		}
+
+		public k2kPoint3D RotateZYX (k2kPoint3D vec)
+		{
+			return Rotate (vec, MatrixZYX);
+		}
+		public k2kPoint3D RotateXYZ (k2kPoint3D vec)
+		{
+			return Rotate (vec, MatrixXYZ);
+		}
+		public k2kPoint3D RotateZYX (double[] vec)
+		{
+			return Rotate (new k2kPoint3D (vec), MatrixZYX);
+		}
+		public k2kPoint3D RotateXYZ (double[] vec)
+		{
+			return Rotate (new k2kPoint3D (vec), MatrixXYZ);
+		}
+		public k2kPoint3D RotateZYX (double x, double y, double z)
+		{
+			return Rotate (new k2kPoint3D (x, y, z), MatrixZYX);
+		}
+		public k2kPoint3D RotateXYZ (double x, double y, double z)
+		{
+			return Rotate (new k2kPoint3D (x, y, z), MatrixXYZ);
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ PlaneEstimation/k2kScreen.cs
@@ -0,0 +1,172 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Drawing;
+
+namespace k2kLib
+{
+
+
+
+    public class k2kScreen
+    {
+        protected k2kScreen(int Width, int Height, k2kMainPlane MainPlane)
+        {
+            this.Width = Width;
+            this.Height = Height;
+            this.MainPlane = MainPlane;
+        }
+
+        public k2kScreen(int Width, int Height, k2kMainPlane MainPlane, k2kPoint3D LeftTop, k2kPoint3D RightBottom, k2kPoint3D YDirection)
+            : this(Width, Height, MainPlane)
+        {
+            var ScreenVec = RightBottom - LeftTop;
+            var HeightVec = ScreenVec.GetComponent(YDirection);
+            var WidthVec = ScreenVec - HeightVec;
+            UnitX = WidthVec / Width;
+            UnitY = HeightVec / Height;
+            this.LeftTop = LeftTop;
+            this.RightBottom = RightBottom;
+            this.RightTop = LeftTop + WidthVec;
+            this.LeftBottom = LeftTop + HeightVec;
+        }
+        public k2kScreen(int Width, int Height, k2kMainPlane MainPlane, Point LeftTop, Point RightBottom, Point LeftBottom)
+            : this(
+                Width, Height, MainPlane,
+                MainPlane.AddZ(LeftTop),
+                MainPlane.AddZ(RightBottom),
+                MainPlane.AddZ(LeftBottom) - MainPlane.AddZ(LeftTop))
+        { }
+        /*public k2kScreen(int Width, int Height, k2kMainPlane MainPlane, k2kPoint3D Head, k2kPoint3D LeftTopHand, k2kPoint3D RightBottomHand, k2kPoint3D Torso)
+            : this(
+                Width, Height, MainPlane,
+                MainPlane.GetIntersection(Head, LeftTopHand),
+                MainPlane.GetIntersection(Head, RightBottomHand),
+                //GetYDirection(MainPlane, Head, Torso)
+                new k2kPoint3D(0,-1,0)
+            )
+        { }*/
+        public k2kScreen(int Width, int Height, k2kMainPlane MainPlane, k2kPoint3D LeftTopHead, k2kPoint3D LeftTopHand, k2kPoint3D RightBottomHead, k2kPoint3D RightBottomHand)
+            : this(
+                Width, Height, MainPlane,
+                MainPlane.GetIntersection(LeftTopHead, LeftTopHand),
+                MainPlane.GetIntersection(RightBottomHead, RightBottomHand),
+                new k2kPoint3D(0, -1, 0)
+            )
+        { }
+        public k2kScreen(int Width, int Height, k2kMainPlane MainPlane, k2kPoint3D LeftTop, k2kPoint3D RightBottom)
+            : this(
+                Width, Height, MainPlane,
+                MainPlane.GetIntersection(LeftTop),
+                MainPlane.GetIntersection(RightBottom),
+                new k2kPoint3D(0, -1, 0)
+            )
+        { }
+
+        protected static k2kPoint3D GetYDirection(k2kMainPlane MainPlane, k2kPoint3D Head, k2kPoint3D Torso)
+        {
+            var BackBone = Torso - Head;
+            return BackBone - BackBone.GetComponentByUnit(MainPlane.NormalVector);
+        }
+
+
+        public int Width { protected set; get; }
+        public int Height { protected set; get; }
+
+        public k2kMainPlane MainPlane { protected set; get; }
+        public k2kPoint3D UnitX { protected set; get; }
+        public k2kPoint3D UnitY { protected set; get; }
+
+        public k2kPoint3D LeftTop { protected set; get; }
+        public k2kPoint3D RightTop { protected set; get; }
+        public k2kPoint3D LeftBottom { protected set; get; }
+        public k2kPoint3D RightBottom { protected set; get; }
+
+        public k2kPoint3D GetIntersection(k2kPoint3D p1, k2kPoint3D p2)
+        {
+            return MainPlane.GetIntersection(p1, p2);
+        }
+        public PointF GetImageCoord0(k2kPoint3D p1, k2kPoint3D p2)
+        {
+            var position = GetIntersection(p1, p2);
+            var vec = position - LeftTop;
+            return new PointF((float)vec.Dot(UnitX), (float)vec.Dot(UnitY));
+        }
+        public PointF GetIntersectionImageCoord(k2kPoint3D p1, k2kPoint3D p2)
+        {
+            return GetImageCoord(GetIntersection(p1, p2));
+        }
+        public PointF GetImageCoord(k2kPoint3D RealPosition)
+        {
+            var vec = RealPosition - LeftTop;
+            var ComponentX = vec.GetComponent(UnitX);
+            var ComponentY = vec.GetComponent(UnitY);
+            return new PointF((float)(ComponentX.Norm / UnitX.Norm), (float)(ComponentY.Norm / UnitY.Norm));
+        }
+    }
+
+    public class k2kMainPlane
+    {
+        public k2kMainPlane(k2kPoint3D[] PointCloud, k2kRobustEstimate estimator)
+        {
+            int i;
+
+            var list = new k2kObservationEquation();
+            for (i = 0; i < PointCloud.Length; ++i)
+                list.Add(PointCloud[i].Z, PointCloud[i].X, PointCloud[i].Y, 1);
+
+            if (list.Count < 5) throw new ArgumentException();
+
+            var Approximation = estimator.GetMaxLikelihood(list).Approximation;
+            if (Approximation == null) throw new ArgumentException();
+
+            a = Approximation[0];
+            b = Approximation[1];
+            c = Approximation[2];
+
+            NormalVector = new k2kPoint3D(a, b, -1) / k2kMath.GetDistance(a, b, -1);
+        }
+
+        public k2kMainPlane(k2kPoint3D[] PointCloud) : this(PointCloud, 100, 4) { }
+        public k2kMainPlane(k2kPoint3D[] PointCloud, int IterationCount, int SampleCount)
+            : this(PointCloud, new k2kLMedS(new k2kModGramSchmidt(), IterationCount, SampleCount)) { }
+
+
+        /// <summary>z = ax + by + c, n = (a, b, -1)</summary>
+        public double a { protected set; get; }
+        /// <summary>z = ax + by + c, n = (a, b, -1)</summary>
+        public double b { protected set; get; }
+        /// <summary>z = ax + by + c, n = (a, b, -1)</summary>
+        public double c { protected set; get; }
+
+        public k2kPoint3D NormalVector { protected set; get; }
+
+
+        public double GetDistance(k2kPoint3D p) { return (a * p.X + b * p.Y - p.Z + c) / Math.Sqrt(a * a + b * b + 1); }
+        public double GetRelativeDistance(k2kPoint3D p) { return a * p.X + b * p.Y - p.Z; }
+
+
+        public k2kPoint3D GetIntersection(k2kPoint3D p1, k2kPoint3D p2) { return k2kMath3D.GetIntersection(p1, p2, a, b, c); }
+        public k2kPoint3D GetIntersection(k2kPoint3D p) { return k2kMath3D.GetIntersection(k2kPoint3D.ZeroPoint, p, a, b, c); }
+
+        public double GetZ(Point p) { return a * p.X + b * p.Y + c; }
+        public k2kPoint3D AddZ(Point p) { return new k2kPoint3D(p.X, p.Y, GetZ(p)); }
+
+
+        static string PlaneFormatString0 = string.Format("Z = {{0{0}X{{1{0}Y{{2{0}", ",8:+0.00;-0.00}");
+
+        public override string ToString() { return ToString(PlaneFormatString0); }
+        public string ToString(string PlaneFormatString)
+        {
+            return string.Format(PlaneFormatString, a, b, c);
+        }
+    }
+
+
+
+
+
+}
